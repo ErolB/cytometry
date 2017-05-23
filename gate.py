8be81c320082c2fb9deca50ca8e18a5470815fd7
erol <eroljb@hotmail.com>
2017-05-23 16:31:11 -0400

    save and load vertex sets

--- gate.py
+++ gate.py
@@ -24,26 +24,72 @@ class LineBuilder:
         self.line.set_data(self.xs, self.ys)
         self.line.figure.canvas.draw()
 
-class FCSData():
+class DataSet():
     data_frame = None
     channel_list = []
 
-    def __init__(self, path, channels=[]):
-        metadata, data = fcsparser.parse(path, reformat_meta=True)
-        if not channels:  # use all channels by default
-            channels = list(metadata['_channels_']['$PnN'].values)
-        # construct data frame
-        self.data_frame = data[channels[0]]  # initialize
-        for field in channels[1:]:
-            self.data_frame = pd.concat([self.data_frame, data[field]], axis=1)
-        self.data_frame.columns = channels
-        # unpack metadata
-        self.channel_list = list(metadata['_channels_']['$PnN'].values)
+    def __init__(self, path=None, data_frame=None, channels=[]):
+        if path:
+            metadata, data = fcsparser.parse(path, reformat_meta=True)
+            if not channels:  # use all channels by default
+                channels = list(metadata['_channels_']['$PnN'].values)
+            # construct data frame
+            self.data_frame = data[channels[0]]  # initialize
+            for field in channels[1:]:
+                self.data_frame = pd.concat([self.data_frame, data[field]], axis=1)
+            self.data_frame.columns = channels
+            # unpack metadata
+            self.channel_list = list(metadata['_channels_']['$PnN'].values)
+        elif data_frame is not None:
+            self.data_frame = data_frame
+            self.channel_list = data_frame.columns
 
-    def apply_manual_gate(self, channel1, channel2, sample_size=1000):
-        # calculate kernel density
-        x = self.data_frame[channel1][:sample_size]
-        y = self.data_frame[channel2][:sample_size]
+
+class Gate():
+    channel_list = []
+
+    def __init__(self, channel_list):
+        self.channel_list = channel_list
+
+    def apply(self):  # defined later
+        pass
+
+class PolygonGate(Gate):
+    vertex_array = None
+
+    def __init__(self, channel_list, path=None):
+        super().__init__(channel_list)
+        if path:
+            vertex_file = open(path, 'r')
+            vertices = []
+            for line in vertex_file.readlines():
+                x, y = line.split()
+                vertices.append([float(x), float(y)])
+            self.vertex_array = np.array(vertices)
+            vertex_file.close()
+
+    def apply(self, data_set, sample_size=1000):
+        x = data_set.data_frame[self.channel_list[0]][:sample_size]
+        y = data_set.data_frame[self.channel_list[1]][:sample_size]
+        if not self.vertex_array:
+            self.define_vertices(x,y)
+        path1 = Path(self.vertex_array)
+        data = np.transpose([x, y])
+        index = path1.contains_points(data)  # Gate in DNA content for singlets
+        # return new data set
+        trimmed_data = pd.DataFrame(data_set.data_frame[:].values[index])
+        trimmed_data.columns = data_set.channel_list
+        x = trimmed_data[self.channel_list[0]][:sample_size]
+        y = trimmed_data[self.channel_list[1]][:sample_size]
+        xy = np.vstack((x, y))
+        z = gaussian_kde(xy)(xy)
+        fig1 = plt.figure(num=1, figsize=(15, 15), dpi=80, facecolor='w', edgecolor='k')
+        ax = fig1.add_subplot(111)
+        ax.scatter(x, y, c=np.log10(z), s=20, edgecolor='')
+        plt.show()
+        return DataSet(data_frame=trimmed_data)  # creates new object
+
+    def define_vertices(self, x, y):
         xy = np.vstack((x, y))
         z = gaussian_kde(xy)(xy)
         # create scatter plot
@@ -56,25 +102,22 @@ class FCSData():
         linebuilder = LineBuilder(line)
         plt.show()
         # select data within gate
-        verts = np.transpose([linebuilder.xs, linebuilder.ys])
-        path1 = Path(verts)
-        data = np.transpose([x, y])
-        index = path1.contains_points(data)  # Gate in DNA content for singlets
-        # return new data set
-        trimmed_data = pd.DataFrame(self.data_frame[:].values[index])
-        trimmed_data.columns = self.channel_list
-        x = trimmed_data[channel1][:sample_size]
-        y = trimmed_data[channel2][:sample_size]
-        xy = np.vstack((x, y))
-        z = gaussian_kde(xy)(xy)
-        fig1 = plt.figure(num=1, figsize=(15, 15), dpi=80, facecolor='w', edgecolor='k')
-        ax = fig1.add_subplot(111)
-        ax.scatter(x, y, c=np.log10(z), s=20, edgecolor='')
-        plt.show()
-        return trimmed_data
+        self.vertex_array = np.transpose([linebuilder.xs, linebuilder.ys])
+
+    def save_vertices(self, path):
+        vertex_file = open(path, 'w')
+        if self.vertex_array is None:
+            print('no vertex set available')
+            return
+        for row in self.vertex_array:
+            line = str(row[0]) + ' ' + str(row[1]) + '\n'
+            vertex_file.write(line)
+        vertex_file.close()
 
 
 if __name__ == '__main__':
-    dataset = FCSData('test.fcs')
-    dataset.apply_manual_gate('FSC-A', 'FSC-H')
+    dataset = DataSet('test.fcs')
+    gate1 = PolygonGate(['FSC-A','SSC-A'])
+    dataset2 = gate1.apply(dataset)
+    gate1.save_vertices('vertex_file.txt')
 
