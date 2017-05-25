b7098c02d8fa772cc3a2e198deff37db72b882a3
erol <eroljb@hotmail.com>
2017-05-25 17:31:40 -0400

    fixed compensation procedure, added function to write .fcs files, started on mutual information calculations

--- compensation.py
+++ compensation.py
@@ -1,5 +1,7 @@
 import numpy as np
-import scipy.linalg as alg
+import fcswrite
+from scipy.integrate import quad
+from scipy.stats import gaussian_kde
 from gate import DataSet
 
 def load_matrix(path):
@@ -13,12 +15,35 @@ def load_matrix(path):
     final_matrix = np.array(matrix, dtype=np.float32)
     return matrix_name, fields, final_matrix
 
-def apply_matrix(data_matrix, comp_matrix):
-    return np.dot(comp_matrix, data_matrix.transpose()).transpose()
+def save_as_fcs(path, data_frame):
+    fcswrite.write_fcs(path, data_frame.columns.values, data_frame.values)
 
-name, fields, spill = load_matrix('CompManual2')
-comp = alg.inv(spill)
-data = DataSet('test.fcs').data_frame[fields].values
-print(data)
-print(data.shape, comp.shape)
-print(apply_matrix(data,comp))
+def apply_matrix(data_matrix, comp_matrix):
+    return np.dot(data_matrix, comp_matrix)
+
+def mutual_info(x_data, y_data):
+    # define limits for integration
+    x_min = min(x_data)
+    x_max = max(x_data)
+    y_min = min(y_data)
+    y_max = max(y_data)
+    # define kernel density functions
+    p_x = gaussian_kde(x_data)
+    p_y = gaussian_kde(y_data)
+    p_xy = gaussian_kde(np.vstack((x_data, y_data)))
+    # perform integration
+    def f(x,y):
+        return p_xy((x,y)) * np.log(p_xy((x,y)) / p_x(x)*p_y(y))
+
+    def f2(y):
+        def f3(x):
+            return f(x,y)
+        return quad(f3, x_min, x_max)[0]
+
+    return quad(f2, y_min, y_max)[0]
+
+
+if __name__ == '__main__':
+    x=[1,2,3]
+    y=[2,3,-99]
+    print(mutual_info(x,y))
--- visualize.py
+++ visualize.py
@@ -3,29 +3,36 @@ from matplotlib import pyplot as plt
 import numpy as np
 from scipy.stats import gaussian_kde
 import scipy.linalg as alg
-from compensation import load_matrix, apply_matrix
+from compensation import load_matrix, apply_matrix, save_as_fcs
 import pandas as pd
+import copy
 
 file_ref = {'BV421-A': 'Single_001_Ly6C_BV421_002.fcs', 'BV510-A': 'Single_001_MHCII_BV480_003.fcs', 'PE-Cy7-A': 'Single_001_CD11b_PECy7_007.fcs',
             'APC-A': 'Single_001_EdU_APC_008.fcs', 'PE ( 561 )-A': 'Single_001_Ly6G_PE_006.fcs',
             'BUV396-A': 'Single_001_CXCR4_BUV496_001.fcs', 'BV711-A': 'Single_001_CD115_BV711_004.fcs', 'FITC-A': 'Single_001_CD117_BB515_005.fcs'}
 
 def create_grid(data_dict, c=10):
-    figure = plt.figure()
-    channel_count = len(file_ref)
+    figure = plt.figure(figsize=(10,10))
+    channel_count = len(data_dict)
     counter = 1
-    for field1 in file_ref.keys():
+    for field1 in data_dict.keys():
         data_set = data_dict[field1]
-        for field2 in file_ref.keys():
+        for field2 in data_dict.keys():
             sub = plt.subplot(channel_count, channel_count, counter)
+            plt.xlim((-10,10))
+            plt.ylim((-10,10))
+            # labeling axes
             if ((channel_count-counter) < counter):
                 sub.set_xlabel(field2)
             if (counter%channel_count == 1):
                 sub.set_ylabel(field1)
-            x = data_set.data_frame[field1].values[:1000]
-            x = [np.arcsinh(item/c) for item in x]
-            y = data_set.data_frame[field2].values[:1000]
+            # take sample
+            x = data_set.data_frame[field2].values[:1000]
+            y = data_set.data_frame[field1].values[:1000]
+            # scaling
+            x = [np.arcsinh(item / c) for item in x]
             y = [np.arcsinh(item/c) for item in y]
+            # generate plot
             xy = np.vstack((x, y))
             print(counter)
             try:
@@ -37,13 +44,46 @@ def create_grid(data_dict, c=10):
             counter += 1
     plt.show()
 
+def generate_samples(channel_count, event_count, spillover_matrix):
+    channels = ['ch'+str(num) for num in range(1, channel_count+1)]
+    frame_list = []
+    for channel_num, channel in enumerate(channels):
+        data_array = []
+        for num in range(event_count):
+            row = []
+            for num2 in range(channel_count):
+                if num2 == channel_num:
+                    row.append(np.random.lognormal())
+                else:
+                    row.append(0)
+            data_array.append(row)
+        data_array = np.array(data_array)
+        print(data_array)
+        data_array = np.dot(data_array, spillover_matrix)
+        data_frame = pd.DataFrame(data_array)
+        data_frame.columns = channels
+        frame_list.append(data_frame)
+    return frame_list
+
 if __name__ == '__main__':
+    '''
+    spillover = [[0.95, 0.03, 0.02], [0, 0.6, 0.4], [0.01, 0.1, 0.89]]
+    frames = generate_samples(3,10,spillover)
+    comp = alg.inv(spillover)
+    data_dict = {'ch1': DataSet(data_frame=frames[0]), 'ch2': DataSet(data_frame=frames[1]), 'ch3': DataSet(data_frame=frames[2])}
+    fields = ['ch1','ch2','ch3']
+    create_grid(data_dict)
+    '''
     name, fields, spill = load_matrix('CompManual2')
     comp = alg.inv(spill)
     data_dict = {name: DataSet('controls/'+file_ref[name]) for name in file_ref.keys()}
-    create_grid(data_dict)
+    #create_grid(data_dict)
+    print(fields)
+
+
     for item in data_dict:
-        new_frame = pd.DataFrame(apply_matrix(data_dict[item].data_frame[fields], comp))
+        new_frame = pd.DataFrame(apply_matrix(data_dict[item].data_frame.loc[:, fields].values, comp))
         new_frame.columns = fields
         data_dict[item].data_frame = new_frame
+        save_as_fcs(item+'.fcs',  copy.deepcopy(new_frame))
     create_grid(data_dict)
