4b58ca3bbc009f54d18ea9aea1baa96a0e99c0ae
erol <eroljb@hotmail.com>
2017-05-26 16:57:44 -0400

    experimenting with mutual information

--- compensation.py
+++ compensation.py
@@ -1,8 +1,13 @@
 import numpy as np
 import fcswrite
-from scipy.integrate import quad
+from scipy.integrate import dblquad
 from scipy.stats import gaussian_kde
 from gate import DataSet
+from sklearn.metrics import mutual_info_score
+
+file_ref = {'BV421-A': 'Single_001_Ly6C_BV421_002.fcs', 'BV510-A': 'Single_001_MHCII_BV480_003.fcs', 'PE-Cy7-A': 'Single_001_CD11b_PECy7_007.fcs',
+            'APC-A': 'Single_001_EdU_APC_008.fcs', 'PE ( 561 )-A': 'Single_001_Ly6G_PE_006.fcs',
+            'BUV396-A': 'Single_001_CXCR4_BUV496_001.fcs', 'BV711-A': 'Single_001_CD115_BV711_004.fcs', 'FITC-A': 'Single_001_CD117_BB515_005.fcs'}
 
 def load_matrix(path):
     matrix = []
@@ -23,27 +28,26 @@ def apply_matrix(data_matrix, comp_matrix):
 
 def mutual_info(x_data, y_data):
     # define limits for integration
-    x_min = min(x_data)
-    x_max = max(x_data)
-    y_min = min(y_data)
-    y_max = max(y_data)
+    x_min = int(min(x_data))
+    x_max = int(max(x_data))
+    y_min = int(min(y_data))
+    y_max = int(max(y_data))
     # define kernel density functions
     p_x = gaussian_kde(x_data)
     p_y = gaussian_kde(y_data)
     p_xy = gaussian_kde(np.vstack((x_data, y_data)))
     # perform integration
     def f(x,y):
-        return p_xy((x,y)) * np.log(p_xy((x,y)) / p_x(x)*p_y(y))
-
-    def f2(y):
-        def f3(x):
-            return f(x,y)
-        return quad(f3, x_min, x_max)[0]
-
-    return quad(f2, y_min, y_max)[0]
+        output = p_xy((x,y)) * np.log(p_xy((x,y)) / (p_x(x)*p_y(y)))
+        if np.isnan(output):
+            output = 0
+        return output
+    summation = 0
+    for x in np.arange(x_min,x_max, float(x_max-x_min)/25):
+        for y in np.arange(y_min,y_max, float(y_max-y_min)/25):
+            summation += f(x,y)
+    return summation
 
 
 if __name__ == '__main__':
-    x=[1,2,3]
-    y=[2,3,-99]
-    print(mutual_info(x,y))
+    print(mutual_info([1,2,558],[9,3,-6]))
